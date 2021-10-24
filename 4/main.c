4c4243e6ee10066f22a1416e438dbba37ec794f2
Mark Bukhner <markbuxner@mail.ru>
2021-10-25 02:38:30 +0700

    Task 4 complete

---
+++ 4/main.c
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct Node {
+    char *data;
+    struct Node* next;
+} Node;
+
+
+Node *create(char *input) {
+    Node *newNode;
+    newNode = malloc(sizeof(Node));
+    newNode->data = malloc(sizeof(char) * (strlen(input) + 1));
+    strcpy(newNode->data, input);
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    char line[BUFSIZ];
+    Node *sentinel, *tail;
+
+    sentinel = malloc(sizeof(Node));
+    sentinel->next = NULL;
+    tail = sentinel;
+
+    while (gets(line)) {
+        if (line[0] == '.')
+            break;
+        tail->next = create(line);
+        tail = tail->next;
+    }
+
+    while(sentinel->next != NULL) {
+        puts(sentinel->next->data);
+        tail = sentinel->next;
+        if (sentinel->data != NULL)
+            free(sentinel->data);
+        free(sentinel);
+        sentinel = tail;
+    }
+    free(sentinel->data);
+    free(sentinel);
+
+    return 0;
+}
