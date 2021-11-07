d42c190fbdb380736daef9e525f26b5e727ff0d6
Mark Bukhner <markbuxner@mail.ru>
2021-11-08 02:05:30 +0700

    Task 5 complete

---
+++ 5/main.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int offsets[1000], lengths[1000];
+    int length = 0, line_number;
+    char c, buf[BUFSIZ];
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Incorrect input file - %s\n", argv[1]);
+        return 1;
+    }
+
+    int i = 0;
+    offsets[++i] = 0;
+    while (read(fd, &c, sizeof(c))) {
+        if (c == '\n') {
+            length++;
+            lengths[i++] = length;
+            offsets[i] = lseek(fd, 0, 1);
+            length = 0;
+        } else {
+            length++;
+        }
+    }
+        
+
+    while (printf("Line: ") && scanf("%d", &line_number)) {
+        if (line_number == 0)
+            return 0;
+
+        lseek(fd, offsets[line_number], 0);
+        if (read(fd, buf, lengths[line_number]))
+            write(1, buf, lengths[line_number]);
+        else
+            fprintf(stderr, "Incorrect line number\n");
+    }
+    return 0;
+}
