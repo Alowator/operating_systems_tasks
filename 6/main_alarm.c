857d314cda464b00eed347fd455101b3e45b6e32
Mark Bukhner <markbuxner@mail.ru>
2021-11-19 03:44:57 +0700

    Add alarm() version

---
+++ 6/main_alarm.c
@@ -0,0 +1,71 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h> 
+
+char buf[BUFSIZ];
+int fd;
+
+void print_file_and_exit(int signo) {
+    lseek(fd, SEEK_SET, 0);
+    int i;
+    while ((i = read(fd, buf, BUFSIZ)) > 0)
+        write(1, buf, i);
+    exit(0);
+}
+
+
+int main(int argc, char *argv[])
+{
+    int offsets[1000], lengths[1000];
+    int length = 0, line_number;
+    char c;
+
+    int tfd = open("/dev/tty", O_RDONLY);
+    if (tfd == -1) {
+        fprintf(stderr, "Cant open /dev/tty\n");
+        return 1;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Incorrect input file - %s\n", argv[1]);
+        return 1;
+    }
+
+    int i = 0;
+    offsets[++i] = 0;
+    while (read(fd, &c, sizeof(c))) {
+        if (c == '\n') {
+            length++;
+            lengths[i++] = length;
+            offsets[i] = lseek(fd, 0, SEEK_CUR);
+            length = 0;
+        } else {
+            length++;
+        }
+    }
+
+    signal(SIGALRM, print_file_and_exit);
+    while (1) {
+        printf("Enter line number:\n");
+        alarm(5);
+        i = read(tfd, buf, BUFSIZ);
+
+        buf[i] = '\0';
+        line_number = atoi(buf);
+        if (line_number <= 0)
+            return 0;
+        lseek(fd, offsets[line_number], 0);
+        if (read(fd, buf, lengths[line_number]))
+            write(1, buf, lengths[line_number]);
+        else
+            fprintf(stderr, "Incorrect line number\n");
+    }
+
+    return 0;
+}
