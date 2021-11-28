2715abb21eb6c745e0562699a1030ce4a78305be
Mark Bukhner <markbuxner@mail.ru>
2021-11-29 02:21:11 +0700

    Init 9

---
+++ 9/main.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+
+int main()
+{
+    pid_t pid, ret;
+    int status;
+
+    if ((pid = fork()) == 0)
+        execl("/bin/cat", "cat", "/etc/passwd", (char *) 0);
+
+    printf("parent: waiting for child: %ld\n", pid);
+
+    /* by adding a wait(2) the parent is guaranteed to finish after its child */
+    ret = wait(&status);
+
+    printf("parent: wait's return value: %ld,", ret);
+    printf("child's status: %d\n", WEXITSTATUS(status));
+    return 0;
+}
